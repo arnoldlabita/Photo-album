d806b009e6f2d2aaa5514bd9b280812ed295738c
arnoldlabita <arnold.labita@gmail.com>
2026-05-23 23:55:37 +0800

    Initial commit

--- albums/admin.py
+++ albums/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Album, Photo
 
-admin.site.register(Album)
-admin.site.register(Photo)
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    search_fields = ('title', 'user__username')
+    list_filter = ('created_at',)
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('caption', 'album', 'uploaded_at')
+    search_fields = ('caption',)
+    list_filter = ('uploaded_at',)
